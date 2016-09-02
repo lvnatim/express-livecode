12c016504f8dd694a1ee90c910c7c1a35e99e734
ChrisVbot <chris.veillette@gmail.com>
2016-09-02 00:59:45 +0000

    sequelize basic implentation functional

---
+++ models/models.js
@@ -0,0 +1,80 @@
+//Sequelize set up and connection 
+var Sequelize = require('sequelize');
+
+var sequelize = new Sequelize('bookstore', 'development', 'development', {
+  host: 'localhost',
+  dialect: 'postgres',
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  },
+});
+
+sequelize
+  .authenticate()
+  .then(function(err){
+    console.log('Connection to postgres has been established successfully.');
+  })
+  .catch(function (err) {
+    console.log('Unable to connect to the database:', err);
+  });
+
+//application-wide models
+var User = sequelize.define('user', {
+  userName: {
+    type: Sequelize.STRING
+  },
+  firstName: {
+    type: Sequelize.STRING
+  },
+  lastName: {
+    type: Sequelize.STRING
+  },
+  email: {
+    type: Sequelize.STRING,
+    validate: {
+      isEmail: true,
+    }
+  },
+  password: {
+    type: Sequelize.STRING
+  }
+});
+
+var Document = sequelize.define('document', {
+  // name: {
+  //   type: Sequelize.STRING
+  // },
+  content: {
+    type: Sequelize.STRING
+  },
+  // owner_id: {
+  //   type: Sequelize.INTEGER
+  // },
+  language: {
+    type: Sequelize.STRING
+  }
+});
+
+var Comment = sequelize.define('comment', {
+  content: {
+    type: Sequelize.STRING
+  }
+});
+
+//For future reference:
+  //This will create a new model called UserDocument with the equivalent foreign keys 
+  //documentID and userID
+  
+// Document.belongstoMany(User, {through: 'UserDocument'});
+// User.belongstoMany(Document, {through: 'UserDocument'});
+// Comment.belongsto(Document);
+// Comment.belongsto(User);
+
+// Test to check connection is correct(must have Document table with something in it to work:
+// Document.findOne().then(function (doc) {
+//     console.log(doc.get(doc));
+// });
+
+module.exports = sequelize;

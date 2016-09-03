78bebba793f557c12a830e0d44ec5ba5613775eb
James Kirkpatrick <jkirkpat16@gmail.com>
2016-09-02 18:04:47 -0700

    updated seed file

--- seeders/20160902201857-populate_database.js
+++ seeders/20160902201857-populate_database.js
@@ -8,9 +8,9 @@ module.exports = {
 
       // Example:
       return queryInterface.bulkInsert('Users', 
-      [{email: 'billy@bob.com', firstName: 'Billy', lastName: 'Bob', password: '123456', createdAt: new Date(), updatedAt: new Date()},
-      {email: 'janedoe@doe.com', firstName: 'Jane', lastName: 'Doe', password: '123456', createdAt: new Date(), updatedAt: new Date()},
-      {email: 'janedoe@doe.com', firstName: 'Jane', lastName: 'Doe', password: '123456', createdAt: new Date(), updatedAt: new Date()}]
+      [{username: 'billyboy', email: 'billy@bob.com', firstName: 'Billy', lastName: 'Bob', password: '123456', createdAt: new Date(), updatedAt: new Date()},
+      {username: 'jdeeze', email: 'janedoe@doe.com', firstName: 'Jane', lastName: 'Doe', password: '123456', createdAt: new Date(), updatedAt: new Date()},
+      {username: 'janetdoe', email: 'janedoe24@doe.com', firstName: 'Jane', lastName: 'Doe', password: '123456', createdAt: new Date(), updatedAt: new Date()}]
       );
   },
 

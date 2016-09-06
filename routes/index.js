34cd7881eae9864a8a9092b5d2e6b43051bb56f1
Tim Lu <tim.lu@ourlog.ca>
2016-09-06 14:29:44 -0700

    Added new profile route

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index');
 });
 
+router.get('/profile', function(req, res, next) {
+  res.send("you're at the profile!");
+})
+
 module.exports = router;

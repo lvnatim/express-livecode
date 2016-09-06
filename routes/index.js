742888e2de376ddfa261989d75dab766ac807952
Tim Lu <tim.lu@ourlog.ca>
2016-09-06 14:33:39 -0700

    Added simple session verification for profile route

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('index');
 });
 
-router.get('/profile', function(req, res, next) {
-  res.send("you're at the profile!");
+router.get('/profile/:id', function(req, res, next) {
+  if(!req.session.user_id){
+    res.send("you're not allowed to access this page!");
+  } else {
+    if(req.session.user_id == req.params.id){
+      res.render('profile');
+    } else {
+      res.send("you're not allowed to access this page!");
+    }
+  }
 })
 
 module.exports = router;

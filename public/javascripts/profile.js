7d87f841ff884b570eade7d6ede3046d7e3a1404
ChrisVbot <chris.veillette@gmail.com>
2016-09-07 01:38:58 +0000

    table added

--- public/javascripts/profile.js
+++ public/javascripts/profile.js
@@ -1,6 +1,13 @@
+
+
 $(".viewDoc").on("click", function(){
   var documentId = $(this).data("liveCode");
   var documentUrl = "/livecode/" + documentId;
   window.open(documentUrl);
 });
+
+$(".document").on("click", function(){
+  $(".document").not(this).removeClass('highlight');
+  $(this).toggleClass('highlight');
+})
 

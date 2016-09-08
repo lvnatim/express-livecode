6353e564907d9438dfdc33f817eb831515ffe93e
ChrisVbot <chris.veillette@gmail.com>
2016-09-08 02:00:15 +0000

    index page displaying sockets

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,23 +1,55 @@
   //todo: implement socket functionality here to 
   //populate splash page with randomly selected
   //live document
+//to do: get view only version
 
 $(function(){
 
-  // var editor = ace.edit("editor")
-  // editor.$blockScrolling = Infinity;
-  // editor.setTheme("ace/theme/monokai");
-  // editor.getSession().setMode("ace/mode/javascript");
+  var editor = ace.edit("editor");
+  editor.setOptions({
+    readOnly: true,
+    highlightActiveLine: true,
+    highlightGutterLine: true
+  })
 
-  // var Range = ace.require("ace/range").Range;
-  // var editSession = editor.getSession();
+  editor.$blockScrolling = Infinity;
+  editor.setTheme("ace/theme/monokai");
+  editor.getSession().setMode("ace/mode/javascript");
 
-  // var onlylisten = false;
- 
- //namespaced socket
- var socket = io('/livecode/1');
- console.log(socket.nsp);
+  var Range = ace.require('ace/range').Range;
+  var editSession = editor.getSession();
 
+  var onlylisten = true;
+
+  var socket = io.connect('http://localhost:3000')
+
+  socket.emit('index-view', function(data){
+    console.log(data);
+  });
+
+    socket.on('EDIT', function(data){
+    var action = data.action;
+    var newText = data.lines.join("\n");
+    var position = data.start;
+
+    var startRow = data.start.row
+    var startColumn = data.start.column
+    var endRow = data.end.row
+    var endColumn = data.end.column
+    var removeRange = new Range(startRow,startColumn,endRow,endColumn);
+
+    if(action==="insert"){
+      onlylisten = true;
+      console.log(position)
+      editSession.insert(position, newText);
+      onlylisten = false;
+    } else if (action==="remove"){
+      onlylisten = true;
+      editSession.remove(removeRange);
+      onlylisten = false;
+    }
+
+  });
 
 
 
